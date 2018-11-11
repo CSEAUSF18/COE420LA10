b32da6242ab8a1e623b85e7669fb3c49e0b21eb6
sradder61 <sradder@aus.edu>
2018-11-11 12:31:11 +0400

    Initial Commit

---
+++ src/la10/LA10.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package la10;
+
+/**
+ *
+ * @author sradder
+ */
+
+
+public class LA10 {
+public void FeatureA()
+{
+ System.out.println("Feature A added)");
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
